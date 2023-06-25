aa91a5e839e7320ac35fc8b017f2f05bcce049af
markmendozadev <markmendozadev@gmail.com>
2023-06-26 07:11:57 +0800

    Added remove,get,patch route

--- controller/projectController.ts
+++ controller/projectController.ts
@@ -33,4 +33,105 @@ const addProject = async (req: Request, res: Response) => {
   }
 };
 
-export { addProject };
+const removeProject = async (req: Request, res: Response) => {
+  const { id } = req.body;
+
+  try {
+    const findProject = await Project.findById(id);
+    if (findProject) {
+      await cloudinary.uploader.destroy(findProject.image.public_id);
+      await Project.findByIdAndDelete(findProject.id);
+      res.status(201).json({
+        success: true,
+        message: "Project Deleted",
+      });
+    } else {
+      res.status(404).json({
+        success: true,
+        message: "Project Not Found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+const getAllProject = async (req: Request, res: Response) => {
+  try {
+    const getProjects = await Project.find();
+    if (getProjects) {
+      res.status(200).json({
+        success: true,
+        projects: getProjects,
+      });
+    } else {
+      res.status(404).json({
+        success: true,
+        message: "Project Not Found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+const getProject = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const getProject = await Project.find({ _id: id });
+    if (getProject) {
+      res.status(200).json({
+        success: true,
+        projects: getProject,
+      });
+    } else {
+      res.status(404).json({
+        success: true,
+        message: "Project Not Found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+const editProject = async (req: Request, res: Response) => {
+  const { id, name, image, description, url, tags } = req.body;
+  try {
+    if (image) {
+      const result = await cloudinary.uploader.upload(image, {
+        folder: "projects",
+      });
+    }
+    const getProject = await Project.updateOne(
+      { _id: id },
+      { name, image, description, url, tags }
+    );
+    if (getProject) {
+      res.status(200).json({
+        success: true,
+      });
+    } else {
+      res.status(404).json({
+        success: true,
+        message: "Project Not Found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+export { addProject, removeProject, getAllProject, getProject, editProject };
--- routes/project.ts
+++ routes/project.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import protect from "../middleware/authMiddleware";
-import { addProject } from "../controller/projectController";
+import {
+  addProject,
+  removeProject,
+  getAllProject,
+  getProject,
+  editProject,
+} from "../controller/projectController";
 const router = express.Router();
 
 router.post("/add", addProject);
+router.delete("/remove", removeProject);
+router.get("/getAll", getAllProject);
+router.get("/getProject/:id", getProject);
+router.patch("/editProject", editProject);
 
 export default router;
